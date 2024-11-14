2fe6724f61d0feb86f41d10d7321893f932aa913
Henrique Letiere <henriquedl2005@gmail.com>
2024-11-14 12:02:46 -0300

    first commit

--- src/main/java/br/com/apifunilaria/projeto/controller/ServicoController.java
+++ src/main/java/br/com/apifunilaria/projeto/controller/ServicoController.java
@@ -20,7 +20,7 @@ import br.com.apifunilaria.projeto.model.Servico;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/novoservico")
+@RequestMapping("/servico")
 public class ServicoController {
 
     @Autowired
